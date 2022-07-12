40334522e714f9143e6653b2fee4c4dc79a55b5b
Leonardo Pinheiro <leoprocha100@gmail.com>
2022-07-12 17:31:20 -0300

    feat(db): create simulated endpoints with MirageJS

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,12 @@ import { createRoot } from "react-dom/client";
 
 import { App } from "./App";
 
+import { makeServer } from "./db";
+
 const container = document.getElementById("root");
 const root = createRoot(container as HTMLDivElement);
+
+makeServer();
 
 root.render(
   <StrictMode>
